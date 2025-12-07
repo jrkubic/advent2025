bfe5c33a5f7dda4da38575e043a1a3c10d3b65da
jrkubic <jrkubic@bgsu.edu>
2025-12-06 21:48:15 -0500

    day6partone

--- DaySix/DaySix.py
+++ DaySix/DaySix.py
@@ -3,53 +3,36 @@
 ##
 
 from os import path
-import sys
 import re
 import pytest
-
-def PartOne(numRange):
-    invalidCount = 0
-    validCount = 0
-    start = int(numRange.split("-")[0])
-    end = int(numRange.split("-")[1])
-    for i in range(start, end+1):
-        print(i)
-        if (bool(re.compile(r'^(.+)\1+$').fullmatch(str(i)))):
-            print("INVALID")
-            invalidCount += 1
-        else:
-            print("VALID")
-            validCount += 1
-    print("invalidCount: ", invalidCount)
-    return invalidCount
+import numpy as np
+import pandas as pd
 
 
+def daySixPartOne (numRange):
+
+    df = pd.DataFrame(numRange, columns=['range_str'])
+
+    quickSplitRanges = df['range_str'].str.split(expand=True)
+    total = 0
+    for column in quickSplitRanges:
+        columnTotal = 0
+        operator = quickSplitRanges[column][-1:]
+        if (operator.values == "+" ):
+            columnTotal = quickSplitRanges[column][:-1].astype('Int64').sum()
+        elif (operator.values == "*" ):
+            columnTotal = quickSplitRanges[column][:-1].astype('Int64').prod()
+       
+        total += columnTotal
+    return total
+   
 if __name__ == '__main__':
-    invalidCount = 0
-    validCount = 0
+    data = []
+    ranges = []
+    ids = []
     with open(path.join(path.dirname(__file__), "input.txt")) as f:
-        for instruction in f:          
-            ...      
-    #         ListSplitByCommas = instruction.split(",")
-    #         for numRange in ListSplitByCommas:
-    #             start = int(numRange.split("-")[0])
-    #             end = int(numRange.split("-")[1])
-    #             for i in range(start, end + 1):
-    #                 if (bool(re.compile(r'^(.+)\1+$').fullmatch(str(i)))):
-    #                     invalidCount += i
-    #                 else:
-    #                     validCount += 1
+        result = daySixPartOne(f)
+    
+    print(result)
 
-    # print(invalidCount)
-        
-# @pytest.mark.parametrize("range, expected", test_data_pt2)
-# def test_repeating_groups(range, expected):
-#     result = PartOne(range)
-#     # print(result)
-#     assert result == expected, f"Failed on {range}"
-
-# # if __name__ == '__main__':
-     
-
-# #         PartOne(f)
         
